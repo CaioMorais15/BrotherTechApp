41dc48723cfb4dc2b9500b4cccc7e436eff33c56
CaioMorais <franciscocaio618@gmail.com>
2024-08-23 16:22:40 -0300

    update

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -4,15 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/Button/button'
 import { Barra } from './components/Barra/Barra'
-
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <Button></Button>
-     <Barra/>
+     <Barra> </Barra>
     </>
   )
 }
