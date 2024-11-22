16c981a9e869ffe1d700ab6f50d82a8c809ab694
Fe-Corazza <felipecorazza09@gmail.com>
2024-11-22 08:47:36 -0300

    Iniciando o projeto fazendo a tela de login

---
+++ MauiApp1/Resources/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Resources.Pages
+{
+    internal class RegisterPage
+    {
+    }
+}
